b9f701ca5aa27919f49e9184b51e81aab4244b1b
Valery Avdeev <vkavdeev@gmail.com>
2022-07-13 23:30:12 +0300

    feat: adding a preloader completing styles

--- src/components/Movies/Movies.js
+++ src/components/Movies/Movies.js
@@ -2,12 +2,14 @@ import './Movies.css';
 import SearchForm from "../SearchForm/SearchForm";
 import MoviesCardList from "../MoviesCardList/MoviesCardList";
 import Still from "../Still/Still";
+import Preloader from "../Preloader/Preloader";
 
 function Movies() {
   return (
     <div className='movies'>
       <SearchForm/>
       <MoviesCardList/>
+      <Preloader/>
       <Still/>
     </div>
   )
